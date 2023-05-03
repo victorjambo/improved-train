d31dd3ad522f4f70ff960a849012352c676a8b2c
victorjambo <victorjambo@live.com>
2023-05-03 21:21:02 +0300

    gcp deployment

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 prod_up: 
 	docker-compose -f docker-compose.yml -f docker-compose.prod.yml up
+prod_up_build: 
+	docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build
 prod_down:
 	docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
 
@@ -7,3 +9,13 @@ dev_up:
 	docker-compose -f docker-compose.yml -f docker-compose.dev.yml up
 dev_down:
 	docker-compose -f docker-compose.yml -f docker-compose.dev.yml down
+
+tag:
+	docker tag supply-chain-client-prod-i gcr.io/jambo-188515/supply-chain:latest
+tag_api:
+	docker tag supply-chain-server-prod-i gcr.io/jambo-188515/supply-chain-api:latest
+
+push:
+	docker push gcr.io/jambo-188515/supply-chain:latest
+push_api:
+	docker push gcr.io/jambo-188515/supply-chain-api:latest
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const url = process.env.BACKEND_API || "http://localhost:4000/";
+
+const App = () => {
+  const [message, setMessage] = useState("");
+  const fetchAPI = useCallback(async () => {
+    await fetch(url)
+      .then((res) => res.json())
+      .then((res) => setMessage(res.message))
+      .catch((err) => console.log({ err }));
+  }, []);
+
+  useEffect(() => {
+    void fetchAPI();
+  }, [fetchAPI]);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,11 +30,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {message || "Learn React"}
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
